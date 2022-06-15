da462cec60b9a1278cf061fcd8686a1e9be3f5c9
V3rn3X <hunterpassaro@gmail.com>
2022-06-16 01:04:04 +0200

    Initial Commit

---
+++ src/PL/SDA/podstawy_java/Klasy.java
@@ -0,0 +1,14 @@
+package PL.SDA.podstawy_java;
+
+import java.util.Scanner;
+
+public class Klasy {
+
+    public static void main(String[] args) {
+
+        Scanner number = new Scanner(System.in);
+        int x = number.nextInt();
+
+        System.out.println(x);
+    }
+}
